078d2b214ab4b2c8fc3df5f897b1cab41e66ee33
Alberto <albe.roldan@outlook.com>
2022-09-11 17:59:55 +0200

    Add attackMonster and attackPlayer methods

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
-const app = Vue.createApp();
+const getRandomeValue = function (min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+};
+
+const app = Vue.createApp({
+  data() {
+    return {
+      playerHealth: 100,
+      monsterHealth: 100,
+    };
+  },
+  methods: {
+    attackMonster() {
+      const attackValue = getRandomeValue(12, 5);
+      this.monsterHealth = this.monsterHealth - attackValue;
+      this.attackPlayer();
+    },
+    attackPlayer() {
+      const attackValue = getRandomeValue(15, 8);
+      this.playerHealth = this.playerHealth - attackValue;
+    },
+  },
+});
 
 app.mount("#game");
