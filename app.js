055f5fa4549debba2f4d953804d37253d3473e0f
Alberto <albe.roldan@outlook.com>
2022-09-12 20:28:02 +0200

    Implement Surrender functionality

--- app.js
+++ app.js
@@ -47,6 +47,9 @@ const app = Vue.createApp({
       this.currentRound = 0;
       this.winner = null;
     },
+    surrender() {
+      this.winner = "monster";
+    },
   },
   computed: {
     monsterBarStyle() {
